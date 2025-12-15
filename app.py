319e6d7b0116c60d5496953ddc530958f1773e03
Vũ Nguyễn Minh Tùng <tung.vnm2421227@sis.hust.edu.vn>
2025-12-15 15:23:52 +0700

    Update total scans from 60 to 52

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 
 DEBUG_MODE = False
-TOTAL_SCANS = 60
+TOTAL_SCANS = 52
 
 redis_url = os.environ.get('REDIS_URL')
 if redis_url:
@@ -127,3 +127,4 @@ def reset_game():
 if __name__ == '__main__':
 
     app.run()
+
